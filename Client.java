7a35b27c5dd072b81ca0e41a453f2b6f9add2083
kevaldsoni <kevaldsoni@gmail.com>
2017-06-18 02:27:52 -0700

    commit 3

--- Client.java
+++ Client.java
@@ -14,26 +14,22 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		TestSites test = new TestSites();
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Scanner sc = new Scanner(System.in);
 		try{
 			while(true){
 				System.out.print("> ");
 				String inp = br.readLine();
+				if("exit".equalsIgnoreCase(inp))
+					break;
 				test.input(inp);
-				System.out.println("Do you want to continue : Type yes or no");
-				if("no".equalsIgnoreCase(sc.next())) break;
-					
 			}
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println("Exception"+e.getMessage());
 		}
 	}
 	
@@ -41,8 +37,12 @@ public class Client {
 }
 
 class TestSites{
+	
+	/**
+	 * This method accepts command and calls the http method based on command
+	 * @param command
+	 */
 	public void input(String command){
-		System.out.println(command);
 		String splitinp[] =  command.split(" ");
 		String primaryCommand = splitinp[0];
 		switch(primaryCommand){
@@ -51,31 +51,69 @@ class TestSites{
 							   for(int i = 1 ; i < splitinp.length-1 ; i++)
 								   urlList.add(splitinp[i].substring(0, splitinp[i].length()-1));
 							    int iteration = Integer.parseInt(splitinp[splitinp.length-1]);
-							    postConnect("startTest",urlList,iteration);break;
+							    postConnect("startTest",urlList,iteration);
+							    break;
 			
-			case "getStatus" : getConnect("testStatus?testHandle="+splitinp[1]);break;
+			case "getStatus" : JSONObject jObj =(JSONObject) getConnect("testStatus?testHandle="+splitinp[1]);
+							   System.out.println("Test "+jObj.get("testHandle")+" is "+jObj.get("status"));
+							   break;
 				
-			case "getResults" : getConnect("testResults?testHandle="+splitinp[1]);break;
+			case "getResults" : JSONArray jArr = (JSONArray)getConnect("testResults?testHandle="+splitinp[1]);
+								if(jArr != null && jArr.size() >0){
+									System.out.println("Site Name \t \t Iterations \t Min Resp Time \t Max Resp Time \t Avg Resp Time \t Test start time \t Test end time");
+									for(int i = 0 ; i < jArr.size() ; i++){
+										JSONObject obj = (JSONObject)jArr.get(i);
+										System.out.print(obj.get("site")+"\t \t");
+										System.out.print(obj.get("iterations")+"\t");
+										System.out.print(obj.get("min")+"\t");
+										System.out.print(obj.get("max")+"\t");
+										System.out.print(obj.get("avg")+"\t");
+										System.out.print(obj.get("avg")+"\t");
+										System.out.print(obj.get("startTestTime")+"\t");
+										System.out.print(obj.get("endTestTime")+"\t");
+										System.out.println();
+									}
+								}
+								break;
 			
-			case "getAll" : getConnect("allTests"); break;
+			case "getAll" : JSONObject obj = (JSONObject) getConnect("allTests");
+							JSONArray jArrhandles = (JSONArray)obj.get("handles");
+							if(jArrhandles != null && jArrhandles.size() >0){
+								System.out.print("Test handles running on server :\t");
+								for(int i = 0 ; i < jArrhandles.size() ; i++){
+									System.out.print(jArrhandles.get(i)+"\t");
+								}
+								System.out.println();
+							}else{
+								System.out.println("Test still running");
+							}
+							break;
 			
 			case "help" : System.out.println("Below commands can be used :");
-						  System.out.println("testSites getStatus getResults getAll"); break;
+						  System.out.println("1. testSites <site1>, <site2>, iterationCount");
+						  System.out.println("2. getStatus <handle>");
+						  System.out.println("3. getResults <handle>");
+						  System.out.println("4. getAll");
+						  System.out.println("5. exit");
+						  break;
 			
-			default: System.out.println("Invalid Command");
+			default: System.out.println("Invalid Command. Type help to get list of valid commands.");
 			
 		}
-		
-		 
 	}
 	
-	private static void getConnect(String handle){
-		String lRespMesg = "";
+	/**
+	 * This method establishes GET connection with server
+	 * @param handle
+	 * @return Object
+	 */
+	private static Object getConnect(String handle){
+		Object obj = null;
 		try{
+			
 		   HttpURLConnection urlconn = null;
 		   handle = "http://localhost:8080/"+handle;
-		   URL url = new URL(handle);	    	   
-		   System.out.println(handle);
+		   URL url = new URL(handle);
 		   urlconn = (HttpURLConnection) url.openConnection();
 		   urlconn.setInstanceFollowRedirects(true);
 		   urlconn.setRequestMethod("GET");
@@ -83,11 +121,9 @@ class TestSites{
 		   urlconn.setConnectTimeout(5000);
 		   urlconn.setReadTimeout(5000);
 		   urlconn.connect();
-		   
 		   int respCode = urlconn.getResponseCode();
-		   
 		   if(respCode!=HttpURLConnection.HTTP_CLIENT_TIMEOUT && respCode==HttpURLConnection.HTTP_OK){
-		   
+			   String lRespMesg = "";
 			   String lStream = "";
 			   BufferedReader inp = new BufferedReader(new InputStreamReader(urlconn.getInputStream()));
 			   while ((lStream = inp.readLine()) != null) {
@@ -95,23 +131,28 @@ class TestSites{
 			   }
 			   inp.close();
 			   urlconn.disconnect();
-		   
-		 		System.out.println("Response :: "+lRespMesg);
-		 		
-		 		JSONParser parser = new JSONParser();
-		 		Object obj = parser.parse(lRespMesg);
-		 		JSONObject jObj = (JSONObject) obj;
-		 		 
+			   if(!"400".equalsIgnoreCase(lRespMesg)){
+			   // Parse JSON response from server
+				   JSONParser parser = new JSONParser();
+			 	   obj = parser.parse(lRespMesg);
+			   }
 		 	}else{
 			    System.out.println("Error in connection");
 			}
 		}catch(Exception e){
 			System.out.println("Failure in connection :"+e.getMessage());
 		}
+		return obj;
 	}
 	
+	/**
+	 * This method establishes POST connect with server
+	 * @param handle
+	 * @param urlList
+	 * @param iteration
+	 */
 	private static void postConnect(String handle, List<String> urlList , int iteration){
-		String lRespMesg = "";
+		
 		try{
 		   HttpURLConnection urlconn = null;
 		   handle = "http://localhost:8080/"+handle;
@@ -131,16 +172,13 @@ class TestSites{
 		   urlconn.setDoOutput(true);
 		   urlconn.setConnectTimeout(5000);
 		   urlconn.setReadTimeout(5000);
-		   
 		   OutputStream os = urlconn.getOutputStream();
 		   OutputStreamWriter wr= new OutputStreamWriter(os,"UTF-8");
 		   wr.write(obj.toJSONString());
 		   wr.flush();
-		   
 		   int respCode = urlconn.getResponseCode();
-		   
 		   if(respCode!=HttpURLConnection.HTTP_CLIENT_TIMEOUT && respCode==HttpURLConnection.HTTP_OK){
-		   
+			   String lRespMesg = "";
 			   String lStream = "";
 			   BufferedReader inp = new BufferedReader(new InputStreamReader(urlconn.getInputStream()));
 			   while ((lStream = inp.readLine()) != null) {
@@ -148,9 +186,11 @@ class TestSites{
 			   }
 			   inp.close();
 			   urlconn.disconnect();
+			   
+			   // Parse JSON response from server
 			   JSONParser parser = new JSONParser();
 		 	   JSONObject jObj = (JSONObject) parser.parse(lRespMesg);
-		 	   System.out.println("Testhandle : "+jObj.get("testHandle"));
+		 	   System.out.println("Test started. Testhandle : "+jObj.get("testHandle"));
 		 	}else{
 			    System.out.println("Error in connection");
 			}
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var async = require('async');
 var fs = require("fs");
-var _ = require("lodash");
+var lodash = require("lodash");
 var server = http.createServer();
 
 //This List saves all testHandles objects.
@@ -23,82 +23,65 @@ server.on('request', function (request, response) {
             	
                 var siteInfo = JSON.parse(data);
                 var siteToTest = siteInfo.sitesToTest;
-                var siteList = [];
                 var iterations = siteInfo.iterations;
                 testHandleCount++;
+                var siteList = [];
                 for(i = 0 ; i < siteToTest.length; i++){
                 	for(j=1 ; j <= iterations ; j++)
                 	siteList.push( siteToTest[i]);
                 }
-                console.log(siteList.length);
-                var testHandle = {
-                    "siteToTest": siteToTest,
-                    "testHandle": testHandleCount.toString(),
-                    "status": "started"
-                }
-                
                 var count = 0;
                 var min = 32767;
                 var max = 0;
                 var avgTime = 0;
                 
                 async.eachSeries(siteList,function(item,callback){
-                		var startTime = (new Date).getTime();
-                		
-                		http.get(item , function(response){
-                			var endTime = (new Date).getTime();
-                			var responsetime = endTime - startTime;
-                			avgTime = _.add(avgTime,responsetime)
-                			if(responsetime < min)
-                				min = responsetime;
-                			if(responsetime > max)
-                				max = responsetime;
-                			count++;
-                			if(count == iterations){
-                				console.log("--------------------Re-----------",avgTime, " ", iterations )
-                				var testHandle = {
-                                        "site": item,
-                                        "iterations": iterations,
-                                        "min": min,
-                                        "max" : max,
-                                        "avg" : ( avgTime ) / iterations,
-                                        "startTestTime" : startTime,
-                                        "endTestTime" : (new Date).getTime()
-                        		}
-                        		testHandles.push(testHandle);
-                        		console.log(JSON.stringify(testHandle));
-                        		startTime = (new Date).getTime();
-                        		count = 0;
-                        		max = -1;
-                        		min = 32767;
-                        		avgTime = 0;
-                			}
-                			callback();
-                	    })
+                	var startTime = (new Date).getTime();
+                	http.get(item , function(response){
+                		var endTime = (new Date).getTime();
+                		var responsetime = endTime - startTime;
+                		avgTime = lodash.add(avgTime,responsetime)
+                		if(responsetime < min)
+                			min = responsetime;
+                		if(responsetime > max)
+                			max = responsetime;
+                		count++;
+                		if(count == iterations){
+                			var testHandle = {
+                                 "site": item,
+                                 "iterations": iterations,
+                                 "min": min,
+                                 "max" : max,
+                                 "avg" : ( avgTime ) / iterations,
+                                 "startTestTime" : startTime,
+                                 "endTestTime" : (new Date).getTime()
+                       		}
+                       		testHandles.push(testHandle);
+                			startTime = (new Date).getTime();
+                        	count = 0;
+                        	max = -1;
+                        	min = 32767;
+                        	avgTime = 0;
+                		}
+                		callback();
+                	})
                 	
               },function(err){
-            	 if (err) {
-                     return console.error(err);
-                 }
-            	console.log("All done"); 
-            	fs.readFile('alltests.txt', function (err, data) {
+            	  if (err) return console.error(err);
+                fs.readFile('alltests.txt', function (err, data) {
                     if (err) {
                        return console.error(err);
                     }
                     var existingData = JSON.parse(data.toString());
                     for(i = 0 ; i < existingData.length; i++){
-                    	console.log(existingData[i].testHandle);
                     	if(testHandleCount == existingData[i].testHandle){
                     		existingData[i].status = 'finished'; 
                     		existingData[i].sitesresults = testHandles;
                     	}
-                    		
                     }
                     testHandles = []
                     fs.writeFile('alltests.txt', JSON.stringify(existingData),function(err) {
-                  	   if (err) {
-                  		      return console.error(err);
-                  	   }
+                    	if (err) return console.error(err);
                      });
                  });
             });
@@ -106,12 +89,9 @@ server.on('request', function (request, response) {
                     "testHandle": testHandleCount.toString(),
                     "status": "started"
             }
-            
             fs.readFile('alltests.txt', function (err, data) {
-                if (err) {
-                   return console.error(err);
-                }
-                var testData
+            	if (err) return console.error(err);
+                var testData;
                 if(data.toString().length > 0){
                 	testData = JSON.parse(data.toString());
 	            }else{
@@ -119,63 +99,48 @@ server.on('request', function (request, response) {
                 }
                 testData.unshift(respObj);
                 fs.writeFile('alltests.txt', JSON.stringify(testData),function(err) {
-	             	   if (err) {
-	             		      return console.error(err);
-	             	   }
+                	if (err) return console.error(err);
 	            });
              });
-            
-            response.write(JSON.stringify(respObj));
-                
+            console.log(JSON.stringify(respObj));
+            response.write(JSON.stringify(respObj), function(){
+            	response.end();
+            });
            }
         }
 
         if (request.method === "GET") {
             var url = request.url;
-            
             if (url.indexOf("/testStatus") !== -1) {
                 var testHandleVal = url.replace("/testStatus?testHandle=", "");
-                console.log(testHandleVal);
                 var status ='';
                 var respObj;
                 async.series([
-                	               function(callback) {
-                                	  fs.readFile('alltests.txt', function (err, data) {
-                                          if (err) {
-                                             return console.error(err);
-                                          }
-                                          var existingData = JSON.parse(data.toString());
-                                          for(i = 0 ; i < existingData.length; i++){
-                                          	if(testHandleVal == existingData[i].testHandle){
-                                          		status = existingData[i].status;
-                                          		break;
-                                          	}                            		
-                                         }
-                                         respObj = {
-                                                  "testHandle": testHandleVal,
-                                                  "status": status
-                                              }
-                                        
-                                         callback();
-                                         
-                                    });
-                                      
-                                  },
-                	              function(callback) {
-                                	 /* response.write(JSON.stringify(respObj), function(err){
-                                		  callback();
-                                  });*/
-                                	  callback();
-                	              },
-                	                 
-                	             ], function (err, result) {
-                	                console.log("in final two");
-                	             });
-                
-                		
-             }
-
-            if (url.indexOf("/testResults") !== -1) {
+                	    function(callback) {
+                             fs.readFile('alltests.txt', function (err, data) {
+                             if (err) {
+                                 return console.error(err);
+                             }
+                             var existingData = JSON.parse(data.toString());
+                             for(i = 0 ; i < existingData.length; i++){
+                                  if(testHandleVal == existingData[i].testHandle){
+                                     status = existingData[i].status;
+                                     respObj = {
+                                             "testHandle": testHandleVal,
+                                             "status": status
+                                     }
+                                     callback();
+                                     break;
+                                  }                            		
+                             }
+                            });
+                         }], function (err, result) {
+                	                response.write(JSON.stringify(respObj), function(){
+                	                	response.end();
+                	                });
+                	     });
+              }
+              if (url.indexOf("/testResults") !== -1) {
                 var testHandleVal = url.replace("/testResults?testHandle=", "");
                 var sitedata;
                 async.series([
@@ -186,60 +151,55 @@ server.on('request', function (request, response) {
                                      }
                                      var existingData = JSON.parse(data.toString());
                                      for(i = 0 ; i < existingData.length; i++){
+                                    	
                                      	if(testHandleVal == existingData[i].testHandle){
-                                     		sitedata = existingData[i].sitesresults;
-                                     		break;
-                                     	}                            		
-                                    }
-                                    
-                                   callback();
+                                     		if(existingData[i].status != "started"){
+	                                     		sitedata = existingData[i].sitesresults;
+	                                     		console.log(sitedata);
+	                                     		callback();
+	                                     		break;
+                                     		}else{
+                                     			response.write("400");
+                                     			response.end();
+            				 	                
+                                     		}
+                                     	}
+                                     }
+                                     
                                });
-                          },
-           	              function(callback) {
-                           	 /* response.write(JSON.stringify(sitedata), function(err){
-                           		  callback();
-                             });*/
-                           	  callback();
-           	              },
-           	                 
-           	             ], function (err, result) {
-           	                console.log("in final two");
+                          }], function (err, result) {
+				                	response.write(JSON.stringify(sitedata), function(){
+				 	                	response.end();
+				 	                });
            	             });
             }
 
             if (url.indexOf("/allTests") !== -1) {
-
+            	var respObj = {}
+            	var tests =[]
             	async.series([
-              	               function(callback) {
+              	              function(callback) {
                               	  fs.readFile('alltests.txt', function (err, data) {
                                         if (err) {
                                            return console.error(err);
                                         }
                                         var existingData = JSON.parse(data.toString());
                                         for(i = 0 ; i < existingData.length; i++){
-                                        	if(testHandleVal == existingData[i].testHandle){
-                                        		sitedata = existingData[i].sitesresults;
-                                        		break;
+                                        	if("started" == existingData[i].status){
+                                        		tests.push(existingData[i].testHandle);
                                         	}                            		
-                                       }
-                                       
-                                      callback();
+                                        }
+                                        callback();
                                   });
-                             },
-              	              function(callback) {
-                              	 /* response.write(JSON.stringify(sitedata), function(err){
-                              		  callback();
-                                });*/
-                              	  callback();
-              	              },
-              	                 
-              	             ], function (err, result) {
-              	                console.log("in final two");
+                              }], function (err, result) {
+            							respObj = {"handles" : tests};
+	            						response.write(JSON.stringify(respObj), function(){
+	            							response.end();
+            						});
               	             });
-                response.write(JSON.stringify(respObj));
-            }
+            	}
         }
-        response.end();
+
     });
 });
 
